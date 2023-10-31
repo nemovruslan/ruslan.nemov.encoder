574b20f4b6c547ccf587d4150482900b86236160
Nemov R. <nemovruslan92@gmail.com>
2023-10-31 20:27:38 +0200

    test

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -3,5 +3,8 @@ package com.javarush;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        while (true) {
+            System.out.println("test");
+        }
     }
 }
