1e200a2d22b8c6b21c4f1d1374149cb5e4675a12
Nemov R. <nemovruslan92@gmail.com>
2023-10-31 20:30:15 +0200

    delete hello world

--- src/main/java/com/javarush/Application.java
+++ src/main/java/com/javarush/Application.java
@@ -2,7 +2,6 @@ package com.javarush;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
         while (true) {
             System.out.println("test");
         }
